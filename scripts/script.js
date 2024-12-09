7ff5548ad5f3f4c29f28b64b4c19feea83792feb
Maxim Loginovskiy <frakiec3600@gmail.com>
2024-12-09 22:44:56 +0500

    feat:dom elements

--- scripts/script.js
+++ scripts/script.js
@@ -28,12 +28,70 @@ const li = document.querySelector('#uniqueLi')
 const liItems = document.querySelectorAll('.list-item')
 li.textContent = 'уникальный  элемент ли'
 
+const ul = document.querySelector('.list')
+ul.style.backgroundColor = 'green'
+// const computedStyles = window.getComputedStyle(ul)
+// const backgroundColor = computedStyles.backgroundColor;
+// console.log(`Текущий цвет фона: ${backgroundColor}`);
 
-const ul = li.parentNode
+const main = document.querySelector('.main')
+const copyUl = ul.cloneNode(true)
+main.append(copyUl)
+setTimeout(() => {
+    copyUl.remove()
+}, 3000);
+
+
+function addLi(text, className) {
+    const newLi = document.createElement('li')
+    newLi.textContent = text
+    // newLi.className = className
+    // newLi.className = 'bold'
+    newLi.classList.add(className)
+    newLi.classList.add('bold')
+    newLi.classList.remove('bold')
+    newLi.classList.toggle('bold')
+    let size = 10
+    // setInterval(()=>{
+    //     size++
+    //     newLi.style.fontSize = size + 'px'
+    // }, 1000)
+    // console.log(newLi.classList.contains('bold'));
+    ul.append(newLi)
+}
+addLi('new el from function', 'orange')
+
+let counter = 0
+// setInterval(() => {
+//     counter++
+//     addLi('new el №' + counter)
+// }, 2000);
 
 const newLi = document.createElement('li')
 newLi.textContent = 'Новый элемент'
+newLi.setAttribute('data-id', 4)
+// console.log(newLi.hasAttribute('data-id'));
+// console.log(newLi.getAttribute('data-id'));
+// console.log(newLi.dataset.id);
 // ul.append(newLi)
 // ul.prepend(newLi)
 ul.insertBefore(newLi, li)
-console.log(ul);
+
+const image = document.createElement('img');
+image.setAttribute('src', './src/BMW.jpg')
+main.prepend(image)
+
+const secondImg = document.getElementById('image')
+secondImg.src = './src/old_bmw.jpg'
+
+const liArr = document.querySelectorAll('.list-item')
+console.log(liArr);
+for(let i = 0; i< liArr.length; i++){
+    if(liArr[i].dataset.id == 1){
+        liArr[i].classList.add('orange')
+        liArr[i].style.removeProperty('border')
+        console.log(liArr[i].textContent);
+    }
+}
+
+
